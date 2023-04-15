5aa0bc2d4d07301702487d574e946127252d5a74
jyyoun1022 <jyyoun1022@naver.com>
2023-04-15 18:59:19 +0900

    SignInService

---
+++ src/main/java/cocode/cocodeMarket/service/sign/SignService.java
@@ -0,0 +1,62 @@
+package cocode.cocodeMarket.service.sign;
+
+import cocode.cocodeMarket.dto.sign.SignInRequest;
+import cocode.cocodeMarket.dto.sign.SignInResponse;
+import cocode.cocodeMarket.dto.sign.SignUpRequest;
+import cocode.cocodeMarket.entity.member.Member;
+import cocode.cocodeMarket.entity.member.RoleType;
+import cocode.cocodeMarket.exception.LoginFailureException;
+import cocode.cocodeMarket.exception.MemberEmailAlreadyExistsException;
+import cocode.cocodeMarket.exception.MemberNicknameAlreadyExistsException;
+import cocode.cocodeMarket.exception.RoleNotFoundException;
+import cocode.cocodeMarket.repository.member.MemberRepository;
+import cocode.cocodeMarket.repository.role.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class SignService {
+
+    private final MemberRepository memberRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+
+    @Transactional
+    public void signUp(SignUpRequest req) {
+        validateSignUpInfo(req);
+        memberRepository.save(SignUpRequest.toEntity(req,
+                roleRepository.findByRoleType(RoleType.ROLE_NORMAL).orElseThrow(RoleNotFoundException::new),
+                passwordEncoder));
+    }
+
+    public SignInResponse signIn(SignInRequest req) {
+        Member member = memberRepository.findByEmail(req.getEmail()).orElseThrow(LoginFailureException::new);
+        validatePassword(req, member);
+        String subject = createSubject(member);
+        String accessToken = tokenService.createAccessToken(subject);
+        String refreshToken = tokenService.createRefreshToken(subject);
+        return new SignInResponse(accessToken, refreshToken);
+    }
+
+    private void validateSignUpInfo(SignUpRequest req) {
+        if(memberRepository.existsByEmail(req.getEmail()))
+            throw new MemberEmailAlreadyExistsException(req.getEmail());
+        if(memberRepository.existsByNickname(req.getNickname()))
+            throw new MemberNicknameAlreadyExistsException(req.getNickname());
+    }
+
+    private void validatePassword(SignInRequest req, Member member) {
+        if(!passwordEncoder.matches(req.getPassword(), member.getPassword())) {
+            throw new LoginFailureException();
+        }
+    }
+    private String createSubject(Member member) {
+        return String.valueOf(member.getId());
+    }
+}
