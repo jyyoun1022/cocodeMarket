b9003c6d1bf059c3b41fbac376bc35fd6ab5e8dd
jyyoun1022 <jyyoun1022@naver.com>
2023-04-15 18:58:25 +0900

    SignIn Request,Response dto

---
+++ src/main/java/cocode/cocodeMarket/dto/sign/SignInRequest.java
@@ -0,0 +1,12 @@
+package cocode.cocodeMarket.dto.sign;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SignInRequest {
+
+    private String email;
+    private String password;
+}
