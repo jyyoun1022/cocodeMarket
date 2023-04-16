1d6836834f29288111a1a2623ba953d18487b42e
jyyoun1022 <jyyoun1022@naver.com>
2023-04-17 00:36:08 +0900

    SignUp,InRequest 기본생성자추가

--- src/main/java/cocode/cocodeMarket/dto/sign/SignInRequest.java
+++ src/main/java/cocode/cocodeMarket/dto/sign/SignInRequest.java
@@ -2,9 +2,11 @@ package cocode.cocodeMarket.dto.sign;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SignInRequest {
 
     private String email;
