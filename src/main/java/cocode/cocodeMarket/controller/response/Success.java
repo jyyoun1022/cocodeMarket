138c877327c6fb3cd91bf135d2182180955fe118
jyyoun1022 <jyyoun1022@naver.com>
2023-04-17 00:36:34 +0900

    Success에서 Getter가 없어서 추가

--- src/main/java/cocode/cocodeMarket/controller/response/Success.java
+++ src/main/java/cocode/cocodeMarket/controller/response/Success.java
@@ -1,8 +1,12 @@
 package cocode.cocodeMarket.controller.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Success<T> implements Result {
     private T data;
 }
