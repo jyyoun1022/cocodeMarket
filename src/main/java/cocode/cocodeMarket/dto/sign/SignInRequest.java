6d5a49753ecf62f7398d2acda6885a5563f16b4b
jyyoun1022 <jyyoun1022@naver.com>
2023-04-17 22:53:54 +0900

    유효성 어노테이션 추가

--- src/main/java/cocode/cocodeMarket/dto/sign/SignInRequest.java
+++ src/main/java/cocode/cocodeMarket/dto/sign/SignInRequest.java
@@ -4,11 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignInRequest {
 
+    @Email(message = "이메일 형식을 맞춰주세요.")
+    @NotBlank(message = "이메일은 필수 값입니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 값입니다.")
     private String password;
 }
--- src/main/java/cocode/cocodeMarket/dto/sign/SignUpRequest.java
+++ src/main/java/cocode/cocodeMarket/dto/sign/SignUpRequest.java
@@ -7,15 +7,34 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignUpRequest {
-    private String email;
+
+    @Email(message = "이메일 형식을 맞춰주세요!")
+    @NotBlank(message = "이메일은 필수 값입니다.")
+    private String email; // 1
+
+    @NotBlank(message = "비밀번호는 필수 값입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+            message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
+
+    @NotBlank(message = "사용자 이름은 필수 값입니다.")
+    @Size(min = 2,message = "사용자 이름은 최소 2글자 이상 입니다.")
+    @Pattern(regexp = "^[A-Za-z가-힣]+$", message = "사용자 이름은 한글 또는 알파벳만 입력해주세요.")
     private String username;
+
+    @NotBlank(message = "닉네임은 필수 값입니다.")
+    @Size(min = 4, message = "닉네임은 최소 4글자 이상 입니다.")
+    @Pattern(regexp = "^[A-Za-z가-힣]+$", message = "닉네임은 한글 또는 알파벳만 입력해주세요.")
     private String nickname;
 
     public static Member toEntity(SignUpRequest request, Role role, PasswordEncoder encoder) {
