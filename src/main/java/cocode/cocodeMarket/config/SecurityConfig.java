842c4676373fae35dc46f2592acd53fb92a2992d
jyyoun1022 <jyyoun1022@naver.com>
2023-04-23 23:43:48 +0900

    SecurityConfig

--- src/main/java/cocode/cocodeMarket/config/SecurityConfig.java
+++ src/main/java/cocode/cocodeMarket/config/SecurityConfig.java
@@ -1,6 +1,13 @@
 package cocode.cocodeMarket.config;
 
+import cocode.cocodeMarket.config.security.CustomAccessDeniedHandler;
+import cocode.cocodeMarket.config.security.CustomAuthenticationEntryPoint;
+import cocode.cocodeMarket.config.security.JwtAuthenticationFilter;
+import cocode.cocodeMarket.config.security.PrincipalUserDetailsService;
+import cocode.cocodeMarket.service.sign.TokenService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,27 +15,42 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity //EnableWebSecurity 안에 @Configuration이 있기때문에 생략
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenService tokenService; // 1
+    private final PrincipalUserDetailsService principalUserDetailsService; // 2
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        super.configure(web);
+        web.ignoring().mvcMatchers("/exception/**"); //3
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable() // 1
-                .formLogin().disable() // 2
-                .csrf().disable() // 3
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 4
+        http.httpBasic().disable()
+                .formLogin().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeRequests()
-                .antMatchers("/**").permitAll(); //5
+                    .authorizeRequests() // 4
+                    .antMatchers(HttpMethod.POST,"/api/sign-in","/api/sign-up").permitAll()
+                    .antMatchers(HttpMethod.GET,"/api/**").permitAll()
+                    .antMatchers(HttpMethod.DELETE,"/api/members/{id}/**").access("@memberGuard.check(#id)")
+                    .anyRequest().hasAnyRole("ADMIN")
+                .and()
+                    .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler()) // 5
+                .and()
+                    .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint()) // 6
+                .and()
+                    .addFilterBefore(new JwtAuthenticationFilter(tokenService,principalUserDetailsService),
+                            UsernamePasswordAuthenticationFilter.class); // 7
     }
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder(); //6
+        return new BCryptPasswordEncoder();
     }
 }
