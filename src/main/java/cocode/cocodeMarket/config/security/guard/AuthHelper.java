b598c87dd8ffe4969f8bae85647e73a828fb3050
jyyoun1022 <jyyoun1022@naver.com>
2023-04-23 22:11:42 +0900

    AuthHelper

---
+++ src/main/java/cocode/cocodeMarket/config/security/guard/AuthHelper.java
@@ -0,0 +1,45 @@
+package cocode.cocodeMarket.config.security.guard;
+
+import cocode.cocodeMarket.config.security.CustomAuthenticationToken;
+import cocode.cocodeMarket.config.security.PrincipalUserDetails;
+import cocode.cocodeMarket.entity.member.RoleType;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+@Log4j2
+public class AuthHelper {
+
+    public boolean isAuthenticated() {
+        return getAuthentication() instanceof CustomAuthenticationToken;
+    }
+    public Long extractMemberId() {
+        return Long.valueOf(getUserDetails().getUserId());
+    }
+    public Set<RoleType> extractMemberRoles() {
+        return getUserDetails().getAuthorities()
+                .stream().map(authority -> authority.getAuthority())
+                .map(strAuth -> RoleType.valueOf(strAuth))
+                .collect(Collectors.toSet());
+    }
+
+    public boolean isAccessTokenType() {
+        return "access".equals(((CustomAuthenticationToken)getAuthentication()).getType());
+    }
+    public boolean isRefreshTokenType() {
+        return "refresh".equals(((CustomAuthenticationToken)getAuthentication()).getType());
+    }
+
+    public PrincipalUserDetails getUserDetails() {
+        return (PrincipalUserDetails)getAuthentication().getPrincipal();
+    }
+    public Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+}
