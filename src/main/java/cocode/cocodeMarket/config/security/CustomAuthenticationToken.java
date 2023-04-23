091ae4710c26e236bba5dac3e51ce50aa037ad53
jyyoun1022 <jyyoun1022@naver.com>
2023-04-23 22:11:50 +0900

    CustomAuthenticationToken

---
+++ src/main/java/cocode/cocodeMarket/config/security/CustomAuthenticationToken.java
@@ -0,0 +1,33 @@
+package cocode.cocodeMarket.config.security;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class CustomAuthenticationToken extends AbstractAuthenticationToken {
+
+    private String type;
+    private PrincipalUserDetails principal;
+
+    public CustomAuthenticationToken(Collection<? extends GrantedAuthority> authorities, String type, PrincipalUserDetails principal) {
+        super(authorities);
+        this.type = type;
+        this.principal = principal;
+        setAuthenticated(true);
+    }
+
+    @Override
+    public Object getCredentials() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return principal;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
