9ae3b45a0cac3c81d47362a54a94d22421cf36ef
jyyoun1022 <jyyoun1022@naver.com>
2023-04-29 20:11:36 +0900

    RefreshToken 발급 dto

---
+++ src/main/java/cocode/cocodeMarket/dto/sign/RefreshTokenResponse.java
@@ -0,0 +1,10 @@
+package cocode.cocodeMarket.dto.sign;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RefreshTokenResponse {
+    private String accessToken;
+}
