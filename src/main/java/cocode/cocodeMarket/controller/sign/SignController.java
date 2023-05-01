189cef9f623bf3f29a08ff0c35c326634b4e7697
jyyoun1022 <jyyoun1022@naver.com>
2023-05-01 18:13:13 +0900

    RefreshToken 발급 API

--- src/main/java/cocode/cocodeMarket/controller/sign/SignController.java
+++ src/main/java/cocode/cocodeMarket/controller/sign/SignController.java
@@ -7,10 +7,7 @@ import cocode.cocodeMarket.service.sign.SignService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -33,5 +30,11 @@ public class SignController {
     @ResponseStatus(HttpStatus.OK)
     public CustomResponse SignIn(@Valid @RequestBody SignInRequest req) { // 3
         return success(signService.signIn(req));
+    }
+
+    @PostMapping("/api/refresh-token")
+    @ResponseStatus(HttpStatus.OK)
+    public CustomResponse refreshToken(@RequestHeader(value = "Authorization") String refreshToken) {
+        return success(signService.refreshToken(refreshToken));
     }
 }
