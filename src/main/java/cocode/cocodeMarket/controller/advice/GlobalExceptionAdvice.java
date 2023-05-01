7fd2deacb56c4720a59f4129dc86d4676ed739f3
jyyoun1022 <jyyoun1022@naver.com>
2023-05-01 18:13:23 +0900

    RefreshToken 관련 헤더 없을시 예외처리

--- src/main/java/cocode/cocodeMarket/controller/advice/GlobalExceptionAdvice.java
+++ src/main/java/cocode/cocodeMarket/controller/advice/GlobalExceptionAdvice.java
@@ -5,6 +5,7 @@ import cocode.cocodeMarket.exception.*;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -64,5 +65,11 @@ public class GlobalExceptionAdvice {
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public CustomResponse accessDeniedException() {
         return CustomResponse.failure(403,"ACCESS_IS_DENIED");
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public CustomResponse missingRequestHeaderException(MissingRequestHeaderException e) {
+        return CustomResponse.failure(404,e.getHeaderName() + "NEED_REQUEST_HEADER");
     }
 }
